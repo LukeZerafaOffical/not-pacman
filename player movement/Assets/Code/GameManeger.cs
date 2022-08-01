ec83679443a57db129b9104b7d74b6e105697bc8
Okan Sakarya <okansakarya79@gmail.com>
2022-08-01 16:32:19 +0300

    player moving script

---
+++ player movement/Assets/Code/GameManeger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManeger : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject[] ghosts;
+    public GameObject[] pacman;
+    public GameObject[] pellets;
+
+}
