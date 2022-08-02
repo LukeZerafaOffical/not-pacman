d9e53f3aa55ef869e5bba813ca191b5110032645
LukeZerafaOffical <luke.zerafa.21@um.edu.mt>
2022-08-02 13:43:31 +0200

    Player can now rotate

--- No Pacman/Assets/codes/PlayerMovement.cs
+++ No Pacman/Assets/codes/PlayerMovement.cs
@@ -8,7 +8,10 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 5f;
     public Rigidbody2D rb;
-    Vector2 movement = Vector2.zero;
+    
+    [SerializeField]
+    private SpriteRenderer sp;
+    Vector2 movement;
     void Update()
     {
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
@@ -16,9 +19,32 @@ public class PlayerMovement : MonoBehaviour
             movement.x = Input.GetAxisRaw("Horizontal");
             movement.y = Input.GetAxisRaw("Vertical");
         }
+        if(movement.x == -1)
+        {
+            sp.flipX = true;
+            sp.gameObject.transform.localRotation = Quaternion.Euler(0,0,0);
+        }
+        else if(movement.x == 1)
+        {
+            sp.flipX = false;
+            sp.gameObject.transform.localRotation = Quaternion.Euler(0,0,0);
+        }
+        if(movement.y == -1)
+        {
+            sp.gameObject.transform.localRotation = Quaternion.Euler(0,0,90);
+            sp.flipX = true;
+        }
+        else if(movement.y == 1)
+        {
+            sp.gameObject.transform.localRotation = Quaternion.Euler(0,0,90);
+            sp.flipX = false;
+        }
+
     }
     void FixedUpdate()
     {
+        
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
+    
 }
